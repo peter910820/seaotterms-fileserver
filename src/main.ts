5fc86f9592b6c28519775f2c54cefb53633337ee
peter910820 <peter910820@gmail.com>
2025-03-29 23:50:58 +0800

    fix: fix endOfLine error

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,7 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 
-// 引入 Materialize CSS
 import "materialize-css/dist/css/materialize.min.css";
-// 引入 Materialize JS
 import "materialize-css/dist/js/materialize.min.js";
 
 createApp(App).use(router).mount("#app");
