01ea9e3aad8008b06c4c549ed562cc05684087dc
peter910820 <peter910820@gmail.com>
2025-03-29 23:11:38 +0800

    feat: init vue.config.js, add materialize-css

--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,9 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 
+// 引入 Materialize CSS
+import "materialize-css/dist/css/materialize.min.css";
+// 引入 Materialize JS
+import "materialize-css/dist/js/materialize.min.js";
+
 createApp(App).use(router).mount("#app");
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,26 @@
 const { defineConfig } = require("@vue/cli-service");
+const TerserPlugin = require("terser-webpack-plugin");
+
 module.exports = defineConfig({
   transpileDependencies: true,
+  productionSourceMap: false, // 禁止生成 source map 檔案
+  configureWebpack: (config) => {
+    config.optimization = {
+      minimize: true, // 啟用程式碼壓縮(此功能有問題)
+      minimizer: [
+        new TerserPlugin({
+          terserOptions: {
+            compress: {
+              drop_console: false, // 移除 console.log(不移除)
+              drop_debugger: true, // 移除 debugger
+            },
+            output: {
+              comments: false, // 移除註解
+            },
+            mangle: true, // 混淆變數名稱
+          },
+        }),
+      ],
+    };
+  },
 });
