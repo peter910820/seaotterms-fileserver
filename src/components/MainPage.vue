3d13cdb92218b8de0a8d60727482f0bc2d311277
peter910820 <peter910820@gmail.com>
2025-03-30 14:02:34 +0800

    test: test directory get api can work

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -1,25 +1,55 @@
 <template>
   <div class="col s12">
     <h1>資源伺服器目錄</h1>
+    {{ directory }}
   </div>
   <div class="col s12 input-field">
     <select>
-      <option class="validate" value="" disabled selected>選擇主題</option>
-      <option :value="1">測試</option>
-      <option :value="1">測試2</option>
-      <option :value="1">測試3</option>
-      <option :value="1">測試4</option>
-      <option :value="1">測試5</option>
+      <option class="validate" value="" disabled selected>選擇資料夾</option>
+      <option v-for="(item, index) in directory" :key="index" :value="item">
+        {{ (item as string).split("\\")[1] }}
+      </option>
     </select>
   </div>
 </template>
 
-<script setup lang="ts">
-import { onMounted } from "vue";
+<script lang="ts">
+import { onMounted, ref, defineComponent } from "vue";
+import { useRouter } from "vue-router";
 import { initMaterialFormSelect } from "@/composables/useMaterial";
+import axios, { AxiosResponse } from "axios";
 
-onMounted(() => {
-  initMaterialFormSelect();
+export default defineComponent({
+  setup() {
+    const router = useRouter();
+    let directory = ref(null);
+
+    onMounted(async () => {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      const getDirectory = async (): Promise<AxiosResponse | undefined> => {
+        const apiUrl = "http://127.0.0.1:3000/api/directory";
+        try {
+          const response = await axios.get(apiUrl);
+          return response;
+          // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        } catch (error: any) {
+          return error.response;
+        }
+      };
+      const response = await getDirectory();
+      if (response && response.status === 200) {
+        console.log(response.data.msg);
+        directory.value = response.data.msg.slice(1);
+      } else if (response) {
+        router.push("/error");
+      } else {
+        router.push("/error");
+      }
+      initMaterialFormSelect();
+    });
+
+    return { directory };
+  },
 });
 </script>
 
