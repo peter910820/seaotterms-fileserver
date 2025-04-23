32c5c8e5b87b4f5e40140849d336086b476cc482
peter910820 <peter910820@gmail.com>
2025-04-23 19:42:54 +0800

    chore: add pinia

--- src/main.ts
+++ src/main.ts
@@ -5,4 +5,6 @@ import router from "./router";
 import "materialize-css/dist/css/materialize.min.css";
 import "materialize-css/dist/js/materialize.min.js";
 
-createApp(App).use(router).mount("#app");
+import { createPinia } from "pinia";
+
+createApp(App).use(router).use(createPinia()).mount("#app");
