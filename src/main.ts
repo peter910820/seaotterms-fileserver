118859acbcd19a36f0ea32b1486207edb285ebfe
peter910820 <peter910820@gmail.com>
2025-05-31 13:34:55 +0800

    feat: add style for FolderPage

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import "materialize-css/dist/css/materialize.min.css";
 import "materialize-css/dist/js/materialize.min.js";
 
 // global stylesheets
+import "./assets/styles/floating.css";
 import "./assets/styles/button.css";
 
 import { createPinia } from "pinia";
