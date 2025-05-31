2ae65eb75d0af293537bb744313c0124073fd363
peter910820 <peter910820@gmail.com>
2025-05-31 11:35:44 +0800

    feat: update FolderPage display and filter feature

--- src/components/FolderPage.vue
+++ src/components/FolderPage.vue
@@ -1,45 +1,25 @@
 <template>
   <div class="row main-block">
-    <h1 class="center">伺服器資料</h1>
+    <h1 class="center">伺服器資源</h1>
     <div class="col s6 file-block input-field">
       <div class="row">
         <div class="col s12 folder" v-for="(item, index) in buttonValue" :key="index" :value="item">
-          <input type="button" class="button-folder" :value="item.split('/')[1]" />
+          <input
+            type="button"
+            class="button-folder"
+            @click="expandDetails(item.split('/')[1])"
+            :value="item.split('/')[1]"
+          />
         </div>
-        <!-- <div class="col s12" v-for="(item, index) in fileData" :key="index" :value="item">
-          <a :href="item">
-            <span>{{ item as string }}</span>
-          </a>
-        </div> -->
       </div>
     </div>
     <div class="col s6 file-block input-field">
       <div class="row">
-        <!-- <div class="col s12" v-for="(item, index) in fileData" :key="index" :value="item">
+        <div class="col s12" v-for="(item, index) in fileData" :key="index" :value="item">
           <a :href="item">
             <span>{{ item as string }}</span>
           </a>
-        </div> -->
-
-        <!-- test -->
-        <!-- <div class="col s12">
-          <a>D:\ScreenShots\screenShotsOld</a>
         </div>
-        <div class="col s12">
-          <a>D:\ScreenShots\screenShotsOld</a>
-        </div>
-        <div class="col s12">
-          <a>D:\ScreenShots\screenShotsOld</a>
-        </div>
-        <div class="col s12">
-          <a>D:\ScreenShots\screenShotsOld</a>
-        </div>
-        <div class="col s12">
-          <a>D:\ScreenShots\screenShotsOld</a>
-        </div>
-        <div class="col s12">
-          <a>D:\ScreenShots\screenShotsOld</a>
-        </div> -->
       </div>
     </div>
   </div>
@@ -61,10 +41,27 @@ export default defineComponent({
     const router = useRouter();
     const fileData = ref(null);
 
+    const expandDetails = async (folder: string) => {
+      const apiUrl = `/api/files?folder=${folder}`;
+      try {
+        const response = await axios.get(apiUrl);
+        if (response && response.status === 200) {
+          fileData.value = response.data.data;
+        } else if (response) {
+          router.push("/error");
+        } else {
+          router.push("/error");
+        }
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      } catch (error: any) {
+        router.push("/error");
+      }
+    };
+
     onMounted(async () => {
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
       const getFile = async (): Promise<AxiosResponse | undefined> => {
-        const apiUrl = "/api/file";
+        const apiUrl = "/api/files";
         try {
           const response = await axios.get(apiUrl);
           return response;
@@ -83,7 +80,7 @@ export default defineComponent({
       }
     });
 
-    return { fileData, buttonValue };
+    return { fileData, buttonValue, expandDetails };
   },
 });
 </script>
